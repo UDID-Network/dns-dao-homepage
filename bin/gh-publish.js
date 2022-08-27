f5e457ad30f11dfe8e744e8f98c17a278386f812
lanbery <lanbery@163.com>
2022-08-27 17:26:35 +0800

    update

--- bin/gh-publish.js
+++ bin/gh-publish.js
@@ -2,7 +2,15 @@
 const { dist, R, ROOT_DIR } = require('./paths');
 const fs = require('fs-extra');
 
-const { PrintOK, PrintError } = require('./loged');
+const { PrintOK, PrintError, PrintInfo } = require('./loged');
+
+const GIT_CMD = {
+  getBranch: 'git rev-parse --abbrev-ref HEAD',
+  getHash: 'git rev-parse --short HEAD',
+  getRemoteURL: 'git remote get-url origin',
+  getRemoteBranches: 'git rev-parse --symbolic-full-name --branches dev-main',
+  gitForceOverride: `git fetch --all && git reset --hard origin/dev-main && git checkout dev-main && git pull`,
+};
 
 main()
   .then(msg => PrintOK(msg))
@@ -13,6 +21,9 @@ async function main() {
   if (!fs.existsSync(srcDist)) throw new Error('Project need build first.');
 
   fs.emptyDirSync(targetDocs);
+
+  await buildProject();
+
   fs.copySync(srcDist, targetDocs, { overwrite: true });
 
   fs.ensureFileSync(publishFile);
@@ -21,6 +32,26 @@ async function main() {
   });
 
   return `Copy success`;
+}
+
+async function buildProject() {
+  const execEnvPath = process.cwd();
+  let commitHash = require('child_process')
+    .execSync(`cd ${execEnvPath} && ${GIT_CMD.getHash}`, {
+      encoding: 'utf-8',
+    })
+    .toString();
+
+  commitHash = commitHash.replace(/\r|\r\n|\n/g, '');
+  PrintInfo('Commit Hash: ', commitHash);
+
+  const buildRes = require('child_process')
+    .execSync(`cd ${execEnvPath} && npm run gh-pages`, {
+      encoding: 'utf-8',
+    })
+    .toString();
+
+  PrintInfo('Commit Hash: ', buildRes);
 }
 
 function checkEnv() {
